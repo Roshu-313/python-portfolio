06cbbfe67cde5740c8ee8d84c0e46dc151e691a8
Roshu-313 <roshanfaisal0509@gmail.com>
2025-12-23 17:05:52 +0500

    Add basic headline scraper

--- project2-web-scraper/scraper.py
+++ project2-web-scraper/scraper.py
@@ -1,1 +1,15 @@
-print("Web Scraper Project Started")
+import requests
+from bs4 import BeautifulSoup
+
+print("Starting web scraper...")
+
+url = "https://news.ycombinator.com/"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+titles = soup.find_all("a", class_="storylink")
+
+for index, title in enumerate(titles, start=1):
+    print(index, title.text)
