77d62406049cd50e5d29c937a43e5f64e23ec187
Roshu-313 <roshanfaisal0509@gmail.com>
2025-12-23 17:23:31 +0500

    Update scraper.py

--- project2-web-scraper/scraper.py
+++ project2-web-scraper/scraper.py
@@ -4,12 +4,14 @@ from bs4 import BeautifulSoup
 print("Starting web scraper...")
 
 url = "https://news.ycombinator.com/"
-
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-titles = soup.find_all("a", class_="storylink")
+titles = soup.select(".titleline a")
 
-for index, title in enumerate(titles, start=1):
-    print(index, title.text)
+with open("headlines.txt", "w", encoding="utf-8") as file:
+    for index, title in enumerate(titles, start=1):
+        file.write(f"{index}. {title.text}\n")
+
+print("Headlines saved to headlines.txt")
